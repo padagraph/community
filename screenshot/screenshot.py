7a5fe1cb09aa574310f496ece220056572aa4f0f
ynnk <ychudy@gmail.com>
2017-02-01 04:26:03 +0100

    handle argparser and size options

--- screenshot/screenshot.py
+++ screenshot/screenshot.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 import time
 import timeit
 
@@ -7,34 +8,39 @@ from PIL import Image
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 
-def getScreenShot(driver, url):
+def getScreenShot(driver, args):
     # http://stackoverflow.com/a/15870708
-
+    
+    path = args.path
+    height = args.height
+    width = args.width
+    url =  "%s/%s/%s#graph" % ( args.host, args.template, args.graph )
+    
     try:
+        
+        
+        driver.set_window_size(height, width)
+        print "resize", driver.get_window_size()
+        
         print "requesting", timeit.default_timer()
+
         driver.get(url)
         print(driver.title)
         
-        
-        print "resize", timeit.default_timer()
-        print driver.get_window_size()
-        
-        driver.set_window_size(800,1200)
-        print driver.get_window_size()
 
         print "waiting", timeit.default_timer()
         element = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//div[@id='vz_threejs_main']/canvas"))
         )
         
-        element = WebDriverWait(driver, 10).until(
-            lambda x : driver.find_element_by_css_selector( "canvas.pdg-renderer")
-        )
+        #element = WebDriverWait(driver, 10).until(
+            #lambda x : driver.find_element_by_css_selector( "canvas.pdg-renderer")
+        #)
 
         #canvas = driver.find_element_by_css_selector( "canvas.pdg-renderer")
         #actions = ActionChains(driver)
@@ -43,35 +49,35 @@ def getScreenShot(driver, url):
         #actions.move_by_offset(-400, -400)
         #actions.double_click()
         #actions.perform()
-        time.sleep(2)
+        time.sleep(5)
         
         print "writing" , timeit.default_timer()
         driver.save_screenshot('screenshot.jpg')
 
         # foxus on graph rendering
         content = driver.find_element_by_id('vz_threejs_main')
-        footer = driver.find_element_by_id('footer')
-
-        location = { k:int(v) for k,v in  content.location.iteritems() }
-        end = { k:int(v) for k,v in  footer.location.iteritems() }
         size = content.size
+        location = { k:int(v) for k,v in  content.location.iteritems() }
 
-        left = location['x'] - 10
-        top = location['y'] - 10
-        right = location['x'] + size['width'] + 10
-        bottom = location['y'] + size['height'] + 42
+        right  = size['width']
+        bottom = size['height'] 
+        left = (width - size['width']) / 2
+        top  = (height - size['height'] ) / 2
 
         left, top, right, bottom = [ int(v) for v in  (left, top, right, bottom) ]
-        
 
+        if args.template == "presentation":
+            left = left - 10
+            top = top - 10
+            right = right + 10
+            bottom = bottom + 42
+            
         print "croping" , timeit.default_timer()
-        print( 'end', end , 'size', size , 'location', location , 'position ltrb', (left, top, right, bottom))
+        print(  'size', size , 'location', location , 'position ltrb', (left, top, right, bottom))
 
         im = Image.open('screenshot.jpg')
         im = im.crop( (left, top, right, bottom))
-
-        print "writing" , timeit.default_timer()
-        im.save('crop.png', "png")
+        im.save(path, "png")
         
     except:
         raise 
@@ -80,22 +86,38 @@ def getScreenShot(driver, url):
 
 usage= """
     screenshot tool for the presentation template:
+    saves image in png format.
     usage :
-    python screenshot.py 'http://localhost:5000/presentation/strains'
+    python screenshot.py 'http://localhost:5000/presentation/strains' crop.png
 """
     
-
-
 def main():
-    if len(sys.argv) == 2:
-        url = sys.argv[1]
-        #driver = webdriver.PhantomJS("./phantomjs-2.1.1-linux-x86_64/bin/phantomjs")
-        #driver = webdriver.Firefox()
-        driver = webdriver.Chrome("chromedriver")
-        getScreenShot(driver, url)
 
-        driver.quit()
-    else :
-        print usage
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("graph", action='store', help="graphname", default=None)
+    parser.add_argument("-o" , action='store', help="output image", dest='path', default='crop.png')
 
-main()
+    parser.add_argument("--width" , action='store', help="width", type=int, default=400)
+    parser.add_argument("--height" , action='store', help="height", type=int, default=400)
+
+    parser.add_argument("-d" , action='store', help="webdriver",  choices=('chromedriver', 'geckodriver', 'ghostdriver'), default='chromedriver')
+    
+    parser.add_argument("--host" , action='store', help="host", default="http://padagraph.io")
+    parser.add_argument("-t" , action='store', dest="template",  choices=('presentation', 'iframe'), default='presentation')
+    parser.add_argument("--background" , action='store', help="background", default='#12AAAA')
+
+    args = parser.parse_args()
+
+    
+    
+    #driver = webdriver.PhantomJS("./phantomjs-2.1.1-linux-x86_64/bin/phantomjs")
+    #driver = webdriver.Firefox()
+    driver = webdriver.Chrome("chromedriver")
+    getScreenShot(driver, args)
+
+    driver.quit()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
