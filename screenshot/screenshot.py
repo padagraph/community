cee37a4ca2119b3bc88ad2305aaf18e5f43de3ca
ynnk <ychudy@gmail.com>
2017-02-01 19:40:26 +0100

    added gviz options

--- screenshot/screenshot.py
+++ screenshot/screenshot.py
@@ -17,17 +17,24 @@ def getScreenShot(driver, args):
     # http://stackoverflow.com/a/15870708
     
     params = {
-        'zoom' : args.zoom,
+        #template
         'color' : args.color,
-        'buttons': 0, # removes play/vote buttons 
-        'labels': 0 if args.no_labels else 1,  # removes grno_labelaph name/attributes 
+        'zoom' : args.zoom,
+        'buttons': 0, # removes play/vote buttons
+        'labels': 0 if args.no_labels else 1,  # removes graph name/attributes 
+        # gviz
+        'vtx_size' : args.vertex_size,
+        'show_text': 0 if args.no_text else 1,  # removes vertex text 
+        'show_nodes': 0 if args.no_nodes else 1,  # removes vertex only 
+        'show_edges': 0 if args.no_edges else 1,  # removes edges 
+        'show_images': 0 if args.no_images else 1,  # removes vertex images 
     }
 
     querystr = "&".join(["%s=%s" % (k,v) for k,v in params.iteritems()])
     url =  "%s/%s/%s?%s#graph" % ( args.host, args.template, args.graph, querystr )
     
     try:
-        driver.set_window_size(args.height, args.width)
+        driver.set_window_size(args.width, args.height)
         print "resize", driver.get_window_size()
         
         print "requesting", url, timeit.default_timer()
@@ -35,8 +42,8 @@ def getScreenShot(driver, args):
         driver.get(url)
         print(driver.title)
         
-        print "waiting", timeit.default_timer()
-        time.sleep(3)
+        print "waiting", timeit.default_timer(), args.wait, " sec"
+        time.sleep( args.wait)
         element = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//div[@id='vz_threejs_main']/canvas"))
         )
@@ -86,11 +93,17 @@ def main():
     parser.add_argument("--width" , action='store', help="width", type=int, default=400)
     parser.add_argument("--height" , action='store', help="height", type=int, default=400)
     parser.add_argument("--color" , action='store', default="12AAAA")
+    parser.add_argument("--vertex-size" , action='store', type=int, default=0, help="(-5, 5) default 0") 
     parser.add_argument("--zoom" , action='store', type=int, default=1200)
     parser.add_argument("--no-labels" , action='store_true', default=False)
+    parser.add_argument("--no-text" , action='store_true', default=False)
+    parser.add_argument("--no-nodes" , action='store_true', default=False)
+    parser.add_argument("--no-edges" , action='store_true', default=False)
+    parser.add_argument("--no-images" , action='store_true', default=False)
 
     parser.add_argument("-d" , action='store', help="webdriver",  choices=('chromedriver', 'geckodriver', 'ghostdriver'), default='chromedriver')
     
+    parser.add_argument("--wait" , action='store', help="seconds to wait for layouts and vertex images to load", default=3., type=float)
     parser.add_argument("--host" , action='store', help="host", default="http://padagraph.io")
     parser.add_argument("-t" , action='store', help="template", dest="template",  choices=('presentation', 'iframe'), default='iframe')
     
